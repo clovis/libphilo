caaabe1c3024eb629ecedfb75ffcca22e4f14706
Richard Whaling <rwhaling@wireless-230-162.uchicago.edu>
2011-01-26 10:44:13 -0600

    new stack class in OHCOVector--moving more behavior from Parser to OV

--- python/philologic/OHCOVector.py
+++ python/philologic/OHCOVector.py
@@ -4,6 +4,113 @@ import os
 import sys
 import codecs
 
+class Record:
+	def __init__(self,type,name,id):
+		self.type = type
+		self.name = name
+		self.id = id
+		self.attrib = {}
+
+	def __str__(self):
+		return str(self.id)
+
+class Stack:
+	def __init__(self,types,parallel):
+		self.v = []
+		self.types = types
+		self.virtual_types = {}
+		self.current_objects = []
+		self.depends_on = {}
+		
+		prior_type = None
+		for type in self.types:
+			self.depends_on[type] = []
+			self.v.append(0)
+			self.current_objects.append(None)
+			if prior_type:
+				self.depends_on[prior_type].append(type)
+			if type[-1].isdigit():
+				v_type = type[:-1]
+				if v_type not in self.virtual_types:
+					self.virtual_types[v_type] = type
+				else:
+					self.virtual_types[v_type].append(type)
+			prior_type = type
+
+		for p_type in parallel:
+			self.v.append(0)
+			self.types.append(p_type)
+			self.current_objects.append(None)
+			self.depends_on[p_type] = []
+			root_type = self.types[0]
+			self.depends_on[root_type].append(p_type)
+
+	def _had_children(self,type): #only legal on real types...just a helper function.
+		has_children = False
+		i = self.types.index(type)
+		dep_types = self.depends_on[type]
+		for d_type in dep_types:
+			d_i = self.types.index(d_type)
+			if self.v[d_i] > 0:
+				has_children = True
+			has_children = has_children or self._had_children(d_type)
+		return has_children
+		
+		
+	def push(self,type,name,value=None):
+		r = []
+		if type in self.virtual_types:
+			real_types = self.virtual_types[type]
+			for r_type in real_types:
+				i = self.types.index(r_type)
+				if self.current_objects[i]:
+					continue
+				else: 
+					break
+			r.extend(self.push(r_type))
+
+		elif type in self.types:
+			i = self.types.index(type) # should this include parallel objcts?  if so, add them to types.  probably.
+			if self._had_children(type):
+				self.pull(type)
+				self.v[i] += 1
+			elif self.current_objects[i]:
+				self.pull(type)
+
+			if value is not None:
+				self.v[i] = value
+			elif self.v[i] == 0:
+				self.v[i] = 1
+
+			r.extend((Record(type,name,self.v[:]),))
+			self.current_objects[i] = r[-1]
+		return r
+		
+	def pull(self,type):
+		r = []
+		if type in self.virtual_types:
+			real_types = self.virtual_types[type]
+			real_types.reverse()
+			for r_type in real_types:
+				i = self.types.index(r_type)
+				if self.current_objects[i]:
+					break
+			r.extend(self.pull(r_type))
+
+		elif type in self.types:
+			i = self.types.index(type)
+			dependents = self.depends_on[type]
+			dependents.reverse()
+			for dep_type in dependents:
+				r.extend(self.pull(dep_type))
+				j = self.types.index(dep_type)
+				self.v[j] = 0
+			if self.current_objects[i]:
+				r.extend((self.current_objects[i],))
+				print r[-1]
+				self.current_objects[i] = None
+				self.v[i] += 1  
+		return r
 
 class OHCOVector:
     """OHCOVector manages all the index arithmetic necessary to construct a PhiloLogic index.
